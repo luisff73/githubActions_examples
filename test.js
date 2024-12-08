7fecc0671f0d070906c2b8726dbd9add97ad2f84
luisff73 <jvrluis@hotmail.com>
2024-12-08 10:14:05 +0100

    Commit actividad 8 modifica readme

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-// describe('Ejemplo Test', () => {
-//     test('should pass', () => {
-//         expect(true).toBe(true);
-//     });
-// });
 describe('Ejemplo Test', () => {
-    test('should fail', () => {
-        expect(true).toBe(false);
+    test('should pass', () => {
+        expect(true).toBe(true);
     });
 });
+// describe('Ejemplo Test', () => {
+//     test('should fail', () => {
+//         expect(true).toBe(false);
+//     });
+// });
